da8d761af9cd5935a5fe9eb1a64d1b0278211f54
Maurice Heumann <MauriceHeumann@gmail.com>
2015-02-13 00:00:42 +0100

    Add a neat header :D

--- server/main.js
+++ server/main.js
@@ -1,7 +1,14 @@
 var server  = require('./server');
+var colors  = require('colors/safe');
 var handler = require('./handler');
 
 process.title = "NodeDayZ";
+
+console.log(colors.grey("###############################################################################"));
+console.log(colors.grey("#                                                                             #"));
+console.log(colors.grey("#            " + colors.green("NodeDayZ Server by momo5502 ( https://momo5502.com )") + "             #"));
+console.log(colors.grey("#                                                                             #"));
+console.log(colors.grey("###############################################################################\n"));
 
 server.useHandler(handler);
 server.start(80);
