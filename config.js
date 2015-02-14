056f71f1ce54acf22bfc37fb52ceb71e79c78a7d
Maurice Heumann <MauriceHeumann@gmail.com>
2015-02-14 13:47:03 +0100

    Restructure handlers.

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ exports.port = 80;              // Server port
 
 exports.whitelist =             // IP/Hostname whitelist
 [
+    //["0.0.0.0"    , 0], // No whitelist
     ["10.0.0.0"   , 16],
     ["192.168.0.0", 24],
     ["localhost"  , 32],
